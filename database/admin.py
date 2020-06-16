f5e4dde7c8e1b5768e232944797673ada4f014b3
Sasha Lukas <sashamati@gmail.com>
2020-06-16 06:48:10 -0700

    done. draggable added needed

--- database/admin.py
+++ database/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 from database.models import Data
 # Register your models here.
 
-admin.site.register(Data, MPTTModelAdmin)
+admin.site.register(Data, DraggableMPTTAdmin)
